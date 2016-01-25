0076e722814cda3bb232758661b66e3308105f41
hatz48 <hatz48@hatena.ne.jp>
2016-01-25 13:44:01 +0900

    context

---
+++ lib/P2JS/Context.pm
@@ -0,0 +1,29 @@
+package P2JS::Context;
+
+sub new {
+    my ($class, %args) = @_;
+    $args{imports} //= [];
+    $args{classes} //= [];
+    return bless \%args, $class;
+}
+
+sub imports {
+    my ($self) = @_;
+    return $self->{imports};
+}
+
+sub classes {
+    my ($self) = @_;
+    return $self->{classes};
+}
+
+sub push_class {
+    my ($self, $class) = @_;
+    push @{$self->{classes}}, $class;
+}
+
+sub current_class {
+    
+}
+
+1;
