e3f9f46e8208198e574318dedf3968434d71df08
hatz48 <hatz48@hatena.ne.jp>
2016-01-27 02:45:37 +0900

    array ref

--- lib/P2JS/Converter/Node/ArrayRef.pm
+++ lib/P2JS/Converter/Node/ArrayRef.pm
@@ -3,7 +3,20 @@ use strict;
 use warnings;
 use parent 'P2JS::Converter::Node';
 
-sub data_node { shift->{data} }
+use P2JS::Converter::Node::Nop;
+
+use P2JS::Node::ArrayLiteral;
+
+sub data_node { shift->{data} // P2JS::Converter::Node::Nop->new; }
+
+sub to_js_ast {
+    my ($self, $context) = @_;
+    return P2JS::Node::ArrayLiteral->new(
+        token => $self->token,
+        data  => $self->data_node->to_js_ast($context),
+        next  => $self->next->to_js_ast($context),
+    );
+}
 
 1;
 
