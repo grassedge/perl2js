58fe794100d26b0a779b4840cf0f94031439ff5a
hatz48 <hatz48@hatena.ne.jp>
2016-01-27 23:40:41 +0900

    hash

--- lib/P2JS/Converter/Node/Hash.pm
+++ lib/P2JS/Converter/Node/Hash.pm
@@ -3,6 +3,33 @@ use strict;
 use warnings;
 use parent 'P2JS::Converter::Node';
 
-sub key { shift->{key} }
+use Compiler::Lexer::Token;
+use P2JS::Converter::Node::Nop;
+use P2JS::Converter::Node::Leaf;
+
+use P2JS::Node::PropertyAccessor;
+
+sub key { shift->{key} // P2JS::Converter::Node::Nop->new; }
+
+sub to_js_ast {
+    my ($self, $context) = @_;
+    my $key;
+    if (ref($self->key) eq 'P2JS::Converter::Node::HashRef') {
+        $key = $self->key->data_node;
+    } else {
+        $key = $self->key;
+    }
+    return P2JS::Node::PropertyAccessor->new(
+        token => $self->token,
+        data  => P2JS::Converter::Node::Leaf->new(
+            token => bless({
+                data => $self->data,
+                name => 'Var',
+            }, 'Compiler::Lexer::Token')
+        )->to_js_ast($context),
+        key   => $key->to_js_ast($context),
+        next  => $self->next->to_js_ast($context),
+    );
+}
 
 1;
