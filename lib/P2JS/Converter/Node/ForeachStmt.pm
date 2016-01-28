5dbddf4f8bf829c03379f7394e56036fb120f166
hatz48 <hatz48@hatena.ne.jp>
2016-01-29 00:42:52 +0900

    for each statement

--- lib/P2JS/Converter/Node/ForeachStmt.pm
+++ lib/P2JS/Converter/Node/ForeachStmt.pm
@@ -3,9 +3,24 @@ use strict;
 use warnings;
 use parent 'P2JS::Converter::Node';
 
-sub cond { shift->{cond} }
-sub itr { shift->{itr} }
-sub true_stmt { shift->{true_stmt} }
+use P2JS::Converter::Node::Nop;
+
+use P2JS::Node::ForofStmt;
+
+sub cond { shift->{cond} // P2JS::Converter::Node::Nop->new; }
+sub itr { shift->{itr} // P2JS::Converter::Node::Nop->new; }
+sub true_stmt { shift->{true_stmt} // P2JS::Converter::Node::Nop->new; }
+
+sub to_js_ast {
+    my ($self, $context) = @_;
+    return P2JS::Node::ForofStmt->new(
+        token => $self->token,
+        cond  => $self->cond->to_js_ast($context),
+        itr   => $self->itr->to_js_ast($context),
+        true_stmt  => $self->true_stmt->to_js_ast($context),
+        next => $self->next->to_js_ast($context),
+    );
+}
 
 1;
 
---
+++ lib/P2JS/Node/ForofStmt.pm
@@ -0,0 +1,46 @@
+package P2JS::Node::ForofStmt;
+
+use strict;
+use warnings;
+use parent qw(P2JS::Node);
+
+use P2JS::Node::Nop;
+
+sub cond {
+    my ($self) = @_;
+    return $self->{cond} // P2JS::Node::Nop->new;
+}
+
+sub itr {
+    my ($self) = @_;
+    return $self->{itr} // P2JS::Node::Nop->new;
+}
+
+sub true_stmt {
+    my ($self) = @_;
+    return $self->{true_stmt} // P2JS::Node::Nop->new;
+}
+
+sub to_javascript {
+    my ($self, $depth) = @_;
+    return (
+        "for (",
+        $self->itr->to_javascript,
+        " of ",
+        $self->cond->to_javascript,
+        ") {\n",
+        ($self->true_stmt->is_nop ?
+         () :
+         ($self->indent($depth + 1),
+          $self->true_stmt->to_javascript($depth + 1),
+          "\n",
+         )
+        ),
+        $self->indent($depth),
+        "}",
+        ($self->next->is_nop ? () : ("\n" . $self->indent($depth))),
+        $self->next->to_javascript($depth),
+    );
+}
+
+1;
