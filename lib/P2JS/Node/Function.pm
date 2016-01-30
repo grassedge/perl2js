a5d78e790e8aa0dcc8489939c695b5b4dae3e8a3
hatz48 <hatz48@hatena.ne.jp>
2016-01-30 23:21:20 +0900

    fix function node.

--- lib/P2JS/Node/Function.pm
+++ lib/P2JS/Node/Function.pm
@@ -4,17 +4,30 @@ use strict;
 use warnings;
 use parent qw(P2JS::Node);
 
+use P2JS::Node::Nop;
+
 sub body {
     my ($self) = @_;
-    return $self->{body};
+    return $self->{body} // P2JS::Node::Nop->new;
 }
 
 sub to_javascript {
     my ($self, $depth) = @_;
     return (
-        $self->indent($depth) . "function " . $self->token->data . "() {\n",
-        $self->indent($depth) . "}\n",
-        $self->next->to_javascript($depth)
+        "export function " . $self->token->data . "() {\n",
+        ($self->body->is_nop ?
+         () :
+         ($self->indent($depth + 1),
+          "if (this !== undefined) { Array.prototype.unshift.call(arguments, this) }\n",
+          $self->indent($depth + 1),
+          $self->body->to_javascript($depth + 1),
+          "\n",
+         )
+        ),
+        $self->indent($depth),
+        "}",
+        ($self->next->is_nop ? () : ("\n" . $self->indent($depth))),
+        $self->next->to_javascript($depth),
     );
 }
 
