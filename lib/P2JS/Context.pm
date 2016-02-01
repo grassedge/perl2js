cac7181bb8fa620fa631fbdc1a67bb21992ca540
hatz48 <hatz48@hatena.ne.jp>
2016-02-02 00:47:52 +0900

    context

--- lib/P2JS/Context.pm
+++ lib/P2JS/Context.pm
@@ -7,6 +7,17 @@ sub new {
     return bless \%args, $class;
 }
 
+sub clone {
+    my ($self, $current) = @_;
+    my $class = ref($self);
+    return bless({
+        %$self,
+        current => $current,
+    }, $class);
+}
+
+####
+
 sub imports {
     my ($self) = @_;
     return $self->{imports};
@@ -30,6 +41,32 @@ sub push_class {
 sub current_class {
     my ($self) = @_;
     return $self->classes->[-1];
+}
+
+####
+
+sub push_sentence {
+    my ($self, $sentence) = @_;
+    $self->current->push_sentence($sentence);
+}
+
+sub root {
+    my ($self, $root) = @_;
+    if ($root) {
+        $self->{root} = $root;
+        $self->current($root);
+    } else {
+        return $self->{root};
+    }
+}
+
+sub current {
+    my ($self, $current) = @_;
+    if ($current) {
+        $self->{current} = $current;
+    } else {
+        return $self->{current};
+    }
 }
 
 1;
