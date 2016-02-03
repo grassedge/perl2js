6793cfce2593c7211ea8559d0aa74fb1199d9641
hatz48 <hatz48@hatena.ne.jp>
2016-02-04 00:00:27 +0900

    pod

--- README.md
+++ README.md
@@ -1,14 +1,45 @@
 # NAME
 
-App::perl2js - It's new $module
+App::perl2js - A module that transpile Perl code to JavaScript as readable as possible.
 
 # SYNOPSIS
 
-    use App::perl2js;
+    use App::perl2js::Converter;
+    print App::perl2js::Converter->new->convert(q[
+        package Hoge;
+        sub hoge {
+            my $self = $_[0];
+            if ($_[1]) {
+                $self->{hoge} = $_[1];
+            } else {
+                return $self->{hoge};
+            }
+        }
+    ]);
+    # ---- output ----
+    # 'use strict';
+    # function print() { console.log.apply(console.log, arguments) }
+    # ... some runtime helplers
+    #
+    # var Hoge = (function() {
+    #     var Hoge = {
+    #         hoge() {
+    #             if (this !== undefined) { Array.prototype.unshift.call(arguments, this) }
+    #             var $self = arguments[0];
+    #             if (arguments[1]) {
+    #                 $self["hoge"] = arguments[1];
+    #             } else {
+    #                 return $self["hoge"]
+    #             }
+    #         },
+    #     }
+    #     return Hoge;
+    # })();
+    # export { Hoge }
 
 # DESCRIPTION
 
-App::perl2js is ...
+App::perl2js is a transpiler from Perl to JavaScript. this module aim to help porting from Perl to JavaScript, not to output runnable code.
 
 # LICENSE
 
--- lib/App/perl2js.pm
+++ lib/App/perl2js.pm
@@ -3,7 +3,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 
 
@@ -14,15 +14,47 @@ __END__
 
 =head1 NAME
 
-App::perl2js - It's new $module
+App::perl2js - A module that transpile Perl code to JavaScript as readable as possible.
 
 =head1 SYNOPSIS
 
-    use App::perl2js;
+    use App::perl2js::Converter;
+    print App::perl2js::Converter->new->convert(q[
+        package Hoge;
+        sub hoge {
+            my $self = $_[0];
+            if ($_[1]) {
+                $self->{hoge} = $_[1];
+            } else {
+                return $self->{hoge};
+            }
+        }
+    ]);
+    # ---- output ----
+    # 'use strict';
+    # function print() { console.log.apply(console.log, arguments) }
+    # ... some runtime helplers
+    #
+    # var Hoge = (function() {
+    #     var Hoge = {
+    #         hoge() {
+    #             if (this !== undefined) { Array.prototype.unshift.call(arguments, this) }
+    #             var $self = arguments[0];
+    #             if (arguments[1]) {
+    #                 $self["hoge"] = arguments[1];
+    #             } else {
+    #                 return $self["hoge"]
+    #             }
+    #         },
+    #     }
+    #     return Hoge;
+    # })();
+    # export { Hoge }
+
 
 =head1 DESCRIPTION
 
-App::perl2js is ...
+App::perl2js is a transpiler from Perl to JavaScript. this module aim to help porting from Perl to JavaScript, not to output runnable code.
 
 =head1 LICENSE
 
@@ -36,4 +68,3 @@ it under the same terms as Perl itself.
 hatz48 E<lt>hatz48@hatena.ne.jpE<gt>
 
 =cut
-
