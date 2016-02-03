d2c4cca525403d8c9f0c38aadaaa2873ebc2a9f1
hatz48 <hatz48@hatena.ne.jp>
2016-02-03 23:24:03 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::perl2js - It's new $module
+
+# SYNOPSIS
+
+    use App::perl2js;
+
+# DESCRIPTION
+
+App::perl2js is ...
+
+# LICENSE
+
+Copyright (C) hatz48.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hatz48 &lt;hatz48@hatena.ne.jp>
---
+++ lib/App/perl2js.pm
@@ -0,0 +1,39 @@
+package App::perl2js;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::perl2js - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::perl2js;
+
+=head1 DESCRIPTION
+
+App::perl2js is ...
+
+=head1 LICENSE
+
+Copyright (C) hatz48.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hatz48 E<lt>hatz48@hatena.ne.jpE<gt>
+
+=cut
+
